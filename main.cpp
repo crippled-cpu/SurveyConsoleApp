e7b088240c5c99c7bb39800ebf73a5fe142eaf5c
Daniel <56933050+crippled-cpu@users.noreply.github.com>
2021-10-03 20:43:17 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,62 @@
+
+#include <iostream> //grab libraries for project
+#include <string>
+
+using namespace std; //stores names for project
+
+
+int main()
+{ //adds space after : to display text seperatly
+    string name; //user inputs name 
+    cout << "Enter your name: ";
+     cin >> name;
+
+     string age;  //user inputs age
+     cout << "Enter your age: ";
+      cin >> age;
+
+      string email; //user inputs email
+      cout << "Enter your email: ";
+      cin >> email;
+
+      string birthdate; //user inputs b-day
+      cout << "Enter your birthdate: ";
+          cin >> birthdate;
+
+          string language; //user inputs language
+          cout << "enter your language: ";
+              cin >> language;
+
+       
+    
+     
+     
+      
+      
+      
+      
+      
+      
+     cout << name << endl; 
+     
+     cout << age << endl;
+
+     cout << email << endl;
+
+     cout << birthdate << endl;
+
+     cout << language << endl;
+
+     cout << "Thank You, you are now done! "; //Displays text to the user telling them they are done
+
+
+    //press any key to continue exiting the console application
+    
+    
+    
+    
+    
+    system("pause"); //pauses the console 
+    return 0; //returns back with 0 errors
+}
+
